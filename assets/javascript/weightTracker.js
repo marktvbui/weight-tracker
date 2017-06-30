0c066d13219b45a1199af132714f891c2ed00f67
Mark Bui <tvyybui@yahoo.com>
2017-06-30 00:01:24 -0700

    got the weight lost to work properly

--- assets/javascript/weightTracker.js
+++ assets/javascript/weightTracker.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
   var database = firebase.database();
   var weight = '';
   var date = '';
+  var weightLost = '';
 
 	$(function () {
 	    $('#datepicker').datepicker();
@@ -28,9 +29,17 @@ $(document).ready(function() {
             return false;
         }
 
+        var previousData = database.ref('weightStatus').limitToLast(1);
+        previousData.on('child_added', function(event){
+            var previousWeight = event.val();
+            weightLost = (previousWeight.weight - weight).toFixed(2);
+            console.log(weightLost);
+        });
+
         currentWeight = {
         weight: weight,
-        date: date
+        date: date,
+        lost: weightLost
         }
 
         database.ref('weightStatus').push(currentWeight);
@@ -41,14 +50,15 @@ $(document).ready(function() {
     function DisplayWeightLost(){
         database.ref('weightStatus').on('child_added', function(snapshot){
 	        var weightStatus = snapshot.val();
-            console.log(weightStatus);
-	        var row = $('<tr>');
+ 	        var row = $('<tr>');
 	        row.append($('<td>').html(weightStatus.date));
 	        row.append($('<td>').html(weightStatus.weight));
+            row.append($('<td>').html(weightStatus.lost));
             $('#weight-table').append(row);
 	    }, function(errorObject){
 	        console.log('read failed: ' + errorObject);
 	        })
     }
+
     DisplayWeightLost();
 });

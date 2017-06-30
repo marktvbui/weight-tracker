e63277aca62e15e61a3957fde59f15292229c211
Mark Bui <tvyybui@yahoo.com>
2017-06-30 00:39:36 -0700

    fixed spacing

--- assets/javascript/weightTracker.js
+++ assets/javascript/weightTracker.js
@@ -15,64 +15,65 @@ $(document).ready(function() {
   var date = '';
   var weightLost = '';
 
-	$(function () {
-	    $('#datepicker').datepicker();
-	});
+  $(function() {
+    $('#datepicker').datepicker();
+  });
 
-    $('#submit-Info').on('click', function(event){
-        event.preventDefault();
-        weight = $('.weight-input').val().trim();
-        date = $('#datepicker').val().trim();
+  $('#submit-Info').on('click', function(event) {
+    event.preventDefault();
+    weight = $('.weight-input').val().trim();
+    date = $('#datepicker').val().trim();
 
-        var numberRegex = /^\d+$/;
-        if (!numberRegex.test(weight)) {
-          return false;
-        }
-        if ((weight === '') || (date === '')) {
-            return false;
-        }
-
-        var previousData = database.ref('weightStatus').limitToLast(1);
-        previousData.on('child_added', function(event){
-            var previousWeight = event.val();
-            weightLost = (previousWeight.weight - weight).toFixed(2);
-        });
-
-        currentWeight = {
-        weight: weight,
-        date: date,
-        lost: weightLost
-        }
-
-        database.ref('weightStatus').push(currentWeight);
-        $('.weight-input').val('');
-        $('#datepicker').val('');
-    })
-
-    function DisplayWeightLost(){
-        database.ref('weightStatus').on('child_added', function(snapshot){
-	        var weightStatus = snapshot.val();
- 	        var row = $('<tr>');
-	        row.append($('<td>').html(weightStatus.date));
-	        row.append($('<td>').html(weightStatus.weight));
-            row.append($('<td>').html(weightStatus.lost));
-            $('#weight-table').append(row);
-	    }, function(errorObject){
-	        console.log('read failed: ' + errorObject);
-	        })
+    var numberRegex = /^\d+$/;
+    if (!numberRegex.test(weight)) {
+      return false;
     }
-      function alertModal(input) {
-        // setting modal to hidden status
-        $('[data-modal-option]').hide();
-        // passes through the input to correctly pick the right modal
-        $('.modal-' + input).show();
-        // shows the correct modal
-        $('#myModal').show();
-        // sets the x button to close the modal, and closes the modal
-        $('#myModal .close').on('click', function() {
-          $('#myModal').hide();
-        })
-      };
+    if ((weight === '') || (date === '')) {
+      return false;
+    }
 
-    DisplayWeightLost();
+    var previousData = database.ref('weightStatus').limitToLast(1);
+    previousData.on('child_added', function(event) {
+      var previousWeight = event.val();
+      weightLost = (previousWeight.weight - weight).toFixed(2);
+    });
+
+    currentWeight = {
+      weight: weight,
+      date: date,
+      lost: weightLost
+    }
+
+    database.ref('weightStatus').push(currentWeight);
+    $('.weight-input').val('');
+    $('#datepicker').val('');
+  })
+
+  function DisplayWeightLost() {
+    database.ref('weightStatus').on('child_added', function(snapshot) {
+      var weightStatus = snapshot.val();
+      var row = $('<tr>');
+      row.append($('<td>').html(weightStatus.date));
+      row.append($('<td>').html(weightStatus.weight));
+      row.append($('<td>').html(weightStatus.lost));
+      $('#weight-table').append(row);
+    }, function(errorObject) {
+      console.log('read failed: ' + errorObject);
+    })
+  }
+
+  function alertModal(input) {
+    // setting modal to hidden status
+    $('[data-modal-option]').hide();
+    // passes through the input to correctly pick the right modal
+    $('.modal-' + input).show();
+    // shows the correct modal
+    $('#myModal').show();
+    // sets the x button to close the modal, and closes the modal
+    $('#myModal .close').on('click', function() {
+      $('#myModal').hide();
+    })
+  };
+
+  DisplayWeightLost();
 });

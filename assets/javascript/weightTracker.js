0b2bfb57cb47c91423c19f3ea8bc7c3bd6265f23
Mark Bui <tvyybui@yahoo.com>
2017-07-07 00:28:11 -0700

    biggest change is setting inputs and labels to the same id

--- assets/javascript/weightTracker.js
+++ assets/javascript/weightTracker.js
@@ -69,13 +69,14 @@ $(document).ready(function() {
     database.ref('weightStatus').on('child_added', function(snapshot) {
       // sets weightStatus variable to current child added to firebase
       var weightStatus = snapshot.val();
+      var key = snapshot.key;
       // setting up table tr, td elements
       var row = $('<tr>');
       row.append($('<td>').html(weightStatus.date));
       row.append($('<td>').html(weightStatus.weight));
       row.append($('<td>').html(weightStatus.lost));
-      row.append($('<td><a href="#">&times;</a></td>'));
-      row.attr(weightStatus.weight);
+      row.append($('<td data-key="' + key + '"><a href="#">&times;</a></td>'));
+      console.log(row);
       // appending row items to the table
       $('#weight-table').append(row);
     }, function(errorObject) {
@@ -97,14 +98,23 @@ $(document).ready(function() {
   };
 
   // this function currently removes the element from the page, working on removing from the database
-  $(function(){
-    $('table').on('click','tr a',function(e){
-       e.preventDefault();
-       var test = database.ref('weightStatus');
-       console.log(test);
-      $(this).parents('tr').remove();
-    });
-  });
+  var remove = function(e){
+    e.preventDefault();
+    var key = $(this).data('key');
+    console.log($(this).data('key'));
+    database.ref('weightStatus').child(key).remove();
+  }
+
+$('table').on('click', 'tr a', remove);
+
+  // $(function(){
+  //   $('table').on('click','tr a',function(e){
+  //      e.preventDefault();
+  //      var test = database.ref('weightStatus');
+  //      test.child(test.key).remove();
+  //     $(this).parents('tr').remove();
+  //   });
+  // });
   // calling function to always display weight lost table
   DisplayWeightLost();
 });

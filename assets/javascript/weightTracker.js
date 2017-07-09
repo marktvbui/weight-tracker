48b3c0dafe0225cdb91a8232667d41edfd48db40
Mark Bui <tvyybui@yahoo.com>
2017-07-09 12:49:28 -0700

    sign in and logout are working

--- assets/javascript/weightTracker.js
+++ assets/javascript/weightTracker.js
@@ -116,13 +116,6 @@ $(document).ready(function() {
     var auth = firebase.auth();
     var promise = auth.signInWithEmailAndPassword(email, password);
     promise.catch(e => console.log(e, message));
-
-    // firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   // ...
-    // });
     $('.modalSignin').hide();
     DisplayWeightLost();
   })
